bf6d939b7e3b04d7b719b5ec3bbe41a0ca8201ca
Sergey Romanov <xxsmotur@gmail.com>
2016-01-03 15:28:50 +0500

    Add test

--- test_prepare.py
+++ test_prepare.py
@@ -8,10 +8,17 @@ class TestPrepare(object):
         assert len(result) == 6
 
     def test_addColumn(self):
-        result = self.item.addColumn('append1', [1,2,3,4,5,6]).toMatrix()
+        data = self.item
+        result = data.addColumn('append1', [1,2,3,4,5,6]).toMatrix()
         assert len(result[0]) == 7
 
     def test_applyColumnEvent(self):
         result = self.item.applyColumnEvent('nice', lambda x: 1 if x == 'A' else 2).toDF()
         expected = {'nice': {0: 1, 1: 2, 2: 2, 3: 2, 4: 2, 5: 2}}
         assert result[['nice']].to_dict() == expected
+
+    def test_mergeColumn(self):
+        result = prepare.Prepare().read('./testdata/test1.csv').mergeColumn(path='./testdata/test2.csv')
+        view = result.toMatrix()
+        assert len(view[0]) == 7
+
